aff2a05666cd353b6d7f0f57600a4ad51fca7a8b
Michael Schlafli <michael.schlafli@gmail.com>
2016-12-17 13:05:17 +0000

    Updated description in main.c

--- main.c
+++ main.c
@@ -1,22 +1,7 @@
-//***************************************************************************************
-//  MSP430 Blink the LED Demo - Software Toggle P1.0
-//
-//  Description; Toggle P1.0 by xor'ing P1.0 inside of a software loop.
-//  ACLK = n/a, MCLK = SMCLK = default DCO
-//
-//                MSP430x5xx
-//             -----------------
-//         /|\|              XIN|-
-//          | |                 |
-//          --|RST          XOUT|-
-//            |                 |
-//            |             P1.0|-->LED
-//
-//  J. Stevenson
-//  Texas Instruments, Inc
-//  July 2011
-//  Built with Code Composer Studio v5
-//***************************************************************************************
+/**
+ * WS2812 driver for MSP430
+ *
+ */
 
 #include <msp430.h>				
 #include <stdint.h>
